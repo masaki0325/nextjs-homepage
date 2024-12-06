2bd8dd92280fad21eadcfa6d623b6e5c9e4bb935
Masaki Sato <masaki@comxia.jp>
2024-12-06 11:06:18 +0900

    ♻️

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,114 +13,124 @@ const Header = () => {
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ type: "spring", duration: 0.5, stiffness: 100 }}
-        className="bg-white fixed top-0 left-0 w-full z-50 h-16 shadow-md"
       >
-        <div className="container mx-auto flex justify-between items-center px-6 py-4">
-          {/* ロゴ */}
-          <Link href="/" className="text-xl font-bold text-gray-800">
-            AAA Corporation
-          </Link>
+        <div className="bg-white fixed top-0 left-0 w-full z-50 h-16 shadow-md">
+          <div className="container mx-auto flex justify-between items-center px-6 py-4">
+            {/* ロゴ */}
+            <Link href="/" className="text-xl font-bold text-gray-800">
+              AAA Corporation
+            </Link>
 
-          {/* ハンバーガーメニューアイコン */}
-          <button
-            className="lg:hidden text-gray-800 focus:outline-none"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            {isMenuOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </button>
+            {/* ハンバーガーメニューアイコン */}
+            <button
+              className="lg:hidden text-gray-800 focus:outline-none"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              {isMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
 
-          {/* デスクトップ用ナビゲーション */}
-          <nav className="hidden lg:flex space-x-6">
-            <Link href="/" className="text-gray-800 hover:text-gray-500">
-              HOME
-            </Link>
-            <Link href="/about" className="text-gray-800 hover:text-gray-500">
-              ABOUT
-            </Link>
-            <Link href="/service" className="text-gray-800 hover:text-gray-500">
-              SERVICE
-            </Link>
-            <Link href="/contact" className="text-gray-800 hover:text-gray-500">
-              CONTACT
-            </Link>
-            <Link href="/privacy" className="text-gray-800 hover:text-gray-500">
-              PRIVACY POLICY
-            </Link>
-          </nav>
-        </div>
-
-        {/* モバイル用メニュー */}
-        {isMenuOpen && (
-          <div className="lg:hidden bg-gray-100 text-gray-800 absolute top-16 left-0 w-full z-40 shadow-md">
-            <nav className="flex flex-col space-y-4 px-6 py-4">
-              <Link
-                href="/"
-                className="block text-gray-800 hover:text-gray-500"
-                onClick={() => setIsMenuOpen(false)}
-              >
+            {/* デスクトップ用ナビゲーション */}
+            <nav className="hidden lg:flex space-x-6">
+              <Link href="/" className="text-gray-800 hover:text-gray-500">
                 HOME
               </Link>
-              <Link
-                href="/about"
-                className="block text-gray-800 hover:text-gray-500"
-                onClick={() => setIsMenuOpen(false)}
-              >
+              <Link href="/about" className="text-gray-800 hover:text-gray-500">
                 ABOUT
               </Link>
               <Link
                 href="/service"
-                className="block text-gray-800 hover:text-gray-500"
-                onClick={() => setIsMenuOpen(false)}
+                className="text-gray-800 hover:text-gray-500"
               >
                 SERVICE
               </Link>
               <Link
                 href="/contact"
-                className="block text-gray-800 hover:text-gray-500"
-                onClick={() => setIsMenuOpen(false)}
+                className="text-gray-800 hover:text-gray-500"
               >
                 CONTACT
               </Link>
               <Link
                 href="/privacy"
-                className="block text-gray-800 hover:text-gray-500"
-                onClick={() => setIsMenuOpen(false)}
+                className="text-gray-800 hover:text-gray-500"
               >
                 PRIVACY POLICY
               </Link>
             </nav>
           </div>
-        )}
+
+          {/* モバイル用メニュー */}
+          {isMenuOpen && (
+            <div className="lg:hidden bg-gray-100 text-gray-800 absolute top-16 left-0 w-full z-40 shadow-md">
+              <nav className="flex flex-col space-y-4 px-6 py-4">
+                <Link
+                  href="/"
+                  className="block text-gray-800 hover:text-gray-500"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  HOME
+                </Link>
+                <Link
+                  href="/about"
+                  className="block text-gray-800 hover:text-gray-500"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  ABOUT
+                </Link>
+                <Link
+                  href="/service"
+                  className="block text-gray-800 hover:text-gray-500"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  SERVICE
+                </Link>
+                <Link
+                  href="/contact"
+                  className="block text-gray-800 hover:text-gray-500"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  CONTACT
+                </Link>
+                <Link
+                  href="/privacy"
+                  className="block text-gray-800 hover:text-gray-500"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  PRIVACY POLICY
+                </Link>
+              </nav>
+            </div>
+          )}
+        </div>
       </motion.header>
     </div>
   );
--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -9,21 +9,14 @@ const HeroSection = () => {
       <div className="container mx-auto px-6 lg:px-16 text-center">
         {/* テキストセクション */}
         <div className="space-y-6 lg:max-w-lg mx-auto">
-          <motion.h1
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
-            className="text-3xl lg:text-4xl font-bold"
-          >
-            未来を動かすコードを。
-          </motion.h1>
           <motion.p
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="text-mg lg:text-xl text-gray-500"
           >
-            最新技術であなたのアイデアを現実にします。
+            <div className="text-mg lg:text-xl text-gray-500">
+              最新技術であなたのアイデアを現実にします。
+            </div>
           </motion.p>
         </div>
 
@@ -32,20 +25,21 @@ const HeroSection = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.6 }}
-          className="flex justify-center flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 mt-6"
         >
-          <Link
-            href="/service"
-            className="px-6 py-3 border border-gray-800 text-gray-800 font-bold rounded-lg hover:bg-gray-800 hover:text-white transition"
-          >
-            私たちのサービス
-          </Link>
-          <Link
-            href="/contact"
-            className="px-6 py-3 bg-orange-500 text-white font-bold rounded-lg hover:bg-orange-600 transition"
-          >
-            お問い合わせ
-          </Link>
+          <div className="flex justify-center flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 mt-6">
+            <Link
+              href="/service"
+              className="px-6 py-3 border border-gray-800 text-gray-800 font-bold rounded-lg hover:bg-gray-800 hover:text-white transition"
+            >
+              私たちのサービス
+            </Link>
+            <Link
+              href="/contact"
+              className="px-6 py-3 bg-orange-500 text-white font-bold rounded-lg hover:bg-orange-600 transition"
+            >
+              お問い合わせ
+            </Link>
+          </div>
         </motion.div>
       </div>
     </section>
